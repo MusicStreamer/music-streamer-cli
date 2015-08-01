450d3c96494b936e0357f3edfb05e3dde6d78fc4
Jiří Konečný <DragonLichcz@gmail.com>
2015-08-01 18:46:42 +0200

    Fix warnings about upper case names
    Constants wasn't upper case.

--- src/auth/constants.rs
+++ src/auth/constants.rs
@@ -1,3 +1,3 @@
-pub const app_id : &'static str = "161875";
-pub const secret : &'static str = "b10a7499aa30948933d9c2a0021d993c";
-pub const redirect_url : &'static str = "http://packetseekers.dvratil.cz/music_lib";
+pub const APP_ID : &'static str = "161875";
+pub const APP_SECRET : &'static str = "b10a7499aa30948933d9c2a0021d993c";
+pub const REDIRECT_URL : &'static str = "http://packetseekers.dvratil.cz/music_lib";
--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -5,5 +5,5 @@ use music_streamer::deezer;
 mod constants;
 
 pub fn get_app_auth_link() -> String {
-    deezer::auth::get_authorize_link(constants::app_id, constants::redirect_url)
+    deezer::auth::get_authorize_link(constants::APP_ID, constants::REDIRECT_URL)
 }
