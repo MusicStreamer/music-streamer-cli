71a8de79c757a15c254f56aec996f40c9fe0c358
Jiří Konečný <DragonLichcz@gmail.com>
2015-08-01 23:40:56 +0200

    Change cli to correspond to newest lib

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,9 +1,12 @@
 extern crate music_streamer;
 
-use music_streamer::deezer;
+use music_streamer::auth::deezer::AuthDeezer;
+use music_streamer::auth::AuthMethods;
 
 mod constants;
 
 pub fn get_app_auth_link() -> String {
-    deezer::auth::get_authorize_link(constants::APP_ID, constants::REDIRECT_URL)
+    let auth_deezer = AuthDeezer::new();
+    
+    auth_deezer.get_authorize_link(constants::APP_ID, constants::REDIRECT_URL)
 }
