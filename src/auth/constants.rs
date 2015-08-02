5bd77cc5a8b136f6105537e05ea274f3dcd6061d
Jiří Konečný <DragonLichcz@gmail.com>
2015-08-02 12:27:04 +0200

    Add licenses to source files

--- src/auth/constants.rs
+++ src/auth/constants.rs
@@ -1,3 +1,18 @@
+// This file is part of music-streamer-cli.
+//
+// music-streamer-cli is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// music-streamer-cli is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with music-streamer-cli.  If not, see <http://www.gnu.org/licenses/>.
+
 pub const APP_ID : &'static str = "161875";
 pub const APP_SECRET : &'static str = "b10a7499aa30948933d9c2a0021d993c";
 pub const REDIRECT_URL : &'static str = "http://packetseekers.dvratil.cz/music_lib";
